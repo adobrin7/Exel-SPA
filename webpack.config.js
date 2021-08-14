ec9508492949a8320e07739ed1eb0bde47d38cd2
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2021-08-14 19:56:03 +0300

    Add environment correct check

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: "development",
