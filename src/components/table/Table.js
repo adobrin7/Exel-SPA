0ed70a2f2cefc3130a73382082743d0e0c854787
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2021-08-20 20:39:28 +0300

    Implement rendering of Table component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,50 +1,10 @@
 import {ExelComponent} from '@/core/ExelComponent';
+import {createTable} from './table.template';
 
 export class Table extends ExelComponent {
   static className = 'exel__table';
 
   toHTML() {
-    return `
-      <div class="exel__table-row row">
-                <div class="row__info">
-
-                </div>
-                <div class="row__data">
-                    <div class="exel__table-column column">
-                        A
-                    </div>
-                    <div class="exel__table-column column">
-                        B
-                    </div>
-                    <div class="exel__table-column column">
-                        C
-                    </div>
-                </div>
-            </div>
-            <div class="exel__table-row row">
-                <div class="row__info">
-                    1
-                </div>
-                <div class="row__data">
-                    <div class="exel__table-cell" contenteditable="true">
-                        2
-                    </div>
-                    <div class="exel__table-cell exel__table-cell_selected">
-                        2
-                    </div>
-                    <div class="exel__table-cell">
-                        2
-                    </div>
-                </div>
-            </div>
-
-            <div class="exel__table-row row">
-                <div class="row__info">
-                    2
-                </div>
-                <div class="row__data">
-                </div>
-            </div>
-    `;
+    return createTable(15);
   }
 }
