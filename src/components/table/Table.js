c24c3d6c5661397f4800c57b5a7c13ec0624ed25
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2021-08-22 17:45:02 +0300

    Add mouse move handler in Table component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,38 @@
 import {ExelComponent} from '@/core/ExelComponent';
 import {createTable} from './table.template';
+import {$} from '@/core/dom';
 
 export class Table extends ExelComponent {
   static className = 'exel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(15);
+  }
+
+  onMousedown(event) {
+    if (!event.target.dataset.resize) {
+      return;
+    }
+    const $resizer = $(event.target);
+    const $parent = $resizer.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+    const cells = document.querySelectorAll(`[data-col="${$parent.data.col}"]`);
+
+    document.onmousemove = e => {
+      const delta = e.pageX - coords.right;
+      const width = coords.width + delta;
+      $parent.$el.style.width = width + 'px';
+      cells.forEach(cell => cell.style.width = width + 'px');
+    };
+
+    document.onmouseup = () => {
+      document.onmousemove = null;
+    };
   }
 }
